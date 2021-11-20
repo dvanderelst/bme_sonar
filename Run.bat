4702ebe44cae520f2c166f714bcc4b9af56ce91a
Lenovo <dieter.vanderelst@fastmail.net>
2021-11-20 09:46:56 -0500

    added script to create bat file
    for running on windows

---
+++ Run.bat
@@ -0,0 +1,3 @@
+call /home/dieter/anaconda3/Scripts/activate.bat /home/dieter/anaconda3
+call activate default
+call python Run.py
---
+++ create_bat_file.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  5 17:15:51 2021
+
+@author: dieter
+"""
+import os
+import sys
+
+def make_bat(python_script, bat_name=None):
+    print('Make bat file:')
+    executable = sys.executable
+
+    # Get path to anaconda3
+    index = executable.find('anaconda3')
+    anaconda = executable[0:index] + 'anaconda3'
+
+    # Get current anaconda environment
+    parts = executable.split(os.sep)
+    index = parts.index('envs')
+    environment = parts[index + 1]
+
+    print('> Executable:', anaconda)
+    print('> Anaconda:', anaconda)
+    print('> Environment:', environment)
+
+    script = '*Scripts*activate.bat'
+    script = script.replace('*', os.sep)
+    
+    line1 = 'call ' + anaconda + script + ' ' + anaconda 
+    line2 = 'call activate ' + environment
+    line3 = 'call python ' + python_script
+    
+    name = python_script.replace('.py', '')
+    name = 'run_' + name + '.bat'
+    if bat_name: name = bat_name
+
+    print('> bat file:', name)
+    
+    f = open(name, 'w')
+    f.write(line1 + '\n')
+    f.write(line2 + '\n')
+    f.write(line3 + '\n')
+    f.close()
+    
+    
+make_bat('Run.py', 'Run.bat')
+
